5bcc0f975b6a6300fc9490888bf03ebc99f9fd31
adarsham-ivoyant <adarsham@ivoyant.com>
2026-03-06 08:14:43 +0530

    created github file

---
+++ github.tf
@@ -0,0 +1,21 @@
+#This file is used to creat github repo 
+terraform {
+  required_providers {
+    github = {
+        source = "integrations/github"
+        version = "~> 5.0"
+    }
+  }
+}
+
+provider "github" {
+  token = "GIT_HUB_PAT"
+}
+
+resource "github_repository" "example" {
+  name        = "example"
+  description = "My awesome codebase"
+
+  visibility = "public"
+
+}
