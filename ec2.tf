35c7d51010271275a607d14b51100d3186426801
adarsham-ivoyant <adarsham@ivoyant.com>
2026-03-09 09:49:22 +0530

    updated files

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,13 @@
-provider "aws" {
-  region     = "ap-south-1"
-  access_key = "PUT-YOUR-ACCESS-KEY-HERE"
-  secret_key = "PUT-YOUR-SECRET-KEY-HERE"
-}
+# provider "aws" {
+#   region     = "us-east-1"
+#   # access_key = "PUT-YOUR-ACCESS-KEY-HERE"
+#   # secret_key = "PUT-YOUR-SECRET-KEY-HERE"
+# }
 
-resource "aws_instance" "myec2" {
-    ami = "ami-051a31ab2f4d498f5"
-    instance_type = "t3.micro"
-    tags = {
-        Name = "ec2"
-    }
-}
+# resource "aws_instance" "myec2" {
+#   ami           = "ami-02dfbd4ff395f2a1b"
+#   instance_type = "t3.micro"
+#   tags = {
+#     Name = "ec2"
+#   }
+# }
--- github.tf
+++ github.tf
@@ -1,21 +1,21 @@
-#This file is used to creat github repo 
-terraform {
-  required_providers {
-    github = {
-        source = "integrations/github"
-        version = "~> 5.0"
-    }
-  }
-}
+# #This file is used to creat github repo 
+# terraform {
+#   required_providers {
+#     github = {
+#         source = "integrations/github"
+#         version = "~> 5.0"
+#     }
+#   }
+# }
 
-provider "github" {
-  token = "GIT_HUB_PAT"
-}
+# provider "github" {
+#   token = "GIT_HUB_PAT"
+# }
 
-resource "github_repository" "example" {
-  name        = "example"
-  description = "My awesome codebase"
+# resource "github_repository" "example" {
+#   name        = "example"
+#   description = "My awesome codebase"
 
-  visibility = "public"
+#   visibility = "public"
 
-}
+# }
